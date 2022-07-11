04061c2796fdeb230a8de59061dbe6c671ad6a00
unknown <kellskelechi@gmail.com>
2022-07-11 22:09:54 +0100

    dependencies

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import  {Stack}  from "@material-ui/core"
+import{CgGym } from "react-icons/Cg"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+     <Link to="/">
+     <CgGym  color="white" fontSize="1.5em"/>
+     </Link>
+    </Stack>
   )
 }
+
 
 export default Navbar
