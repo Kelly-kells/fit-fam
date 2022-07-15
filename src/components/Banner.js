06ee91eb975dc7c5bc45b52f269072ebb25108c0
unknown <kellskelechi@gmail.com>
2022-07-15 23:00:03 +0100

    Headin

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { Button } from "@mui/material";
-import BannerImage from '../assets/BannerImage.jpg'
+import BannerImage from "../assets/BannerImage.jpg";
 
 const Banner = () => {
   return (
     <Box
-      sx={{ mt: { lg: "200px", xs: "70px" }, ml: { sm: "50px" } }}
+      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
       position="relative"
-      p="200px"
+      p="20px"
     >
       <Typography color="#f94144" fontWeight="600" fontSize="26px">
         Stay Fit
       </Typography>
-      <Typography fontWeight={700}
-      sx={{fontSize:{lg:'40px', sx: "36px"}}} >
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "40px", sx: "36px" } }}
+      >
         We Can Give A Shape Of <br /> Your Body Here!
-      </Typography >
-      <Typography lineHeight='2' fontSize='1.5rem' mb={3}>Check out various exercises</Typography>
+      </Typography>
+      <Typography lineHeight="2" fontSize="1.5rem" mb={3}>
+        Check out various exercises
+      </Typography>
       <Button variant="contained" color="error" href="#exercise">
-  Explore
-</Button>
-<img  src={BannerImage} alt="Banner"   className="BannerImg"/>
+        Explore
+      </Button>
+      <img src={BannerImage} alt="Banner" className="BannerImg" />
     </Box>
   );
 };
