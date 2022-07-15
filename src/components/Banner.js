3745dde25765f8798c560c8d6b83c00c6bda84a9
unknown <kellskelechi@gmail.com>
2022-07-15 14:37:28 +0100

    banner-section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
+import { Button } from "@mui/material";
+import BannerImage from '../assets/BannerImage.jpg'
 
 const Banner = () => {
   return (
@@ -11,10 +13,15 @@ const Banner = () => {
       <Typography color="#f94144" fontWeight="600" fontSize="26px">
         Stay Fit
       </Typography>
-      <Typography fontWeight={700} >
+      <Typography fontWeight={700}
+      sx={{fontSize:{lg:'40px', sx: "36px"}}} >
         We Can Give A Shape Of <br /> Your Body Here!
-      </Typography>
-      <Typography>Check out various exercises</Typography>
+      </Typography >
+      <Typography lineHeight='2' fontSize='1.5rem' mb={3}>Check out various exercises</Typography>
+      <Button variant="contained" color="error" href="#exercise">
+  Explore
+</Button>
+<img  src={BannerImage} alt="Banner"   className="BannerImg"/>
     </Box>
   );
 };
