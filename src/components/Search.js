1f5ed3f7b25fe9a0089d624feeec2382878d7bde
unknown <kellskelechi@gmail.com>
2022-07-17 20:30:42 +0100

    search-section

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { Box, Stack, TextField, Typography, Button } from "@mui/material";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={600}
+        sx={{ fontSize: { lg: "3rem", sx: "2.5rem" } }}
+        mb="1rem"
+        textAlign="center"
+      >
+        Check Out These Awesome <br />
+        Exercises .
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField />
+      </Box>
+    </Stack>
+  );
+};
 
-export default Search
+export default Search;
