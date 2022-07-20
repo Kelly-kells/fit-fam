3fd572cfde6d1cce63c0e9a0c076f88d17a44319
unknown <kellskelechi@gmail.com>
2022-07-20 22:55:07 +0100

    logic

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,7 @@ import { exerciseOptions, fetchData } from "../Utilities/fetchData";
 
 const Search = () => {
   const [search, setSearch] = useState("");
+  const [exercises, setExercises]=useState([])
   const handleSearch = async () => {
    if (search) {
     const exerciseData= await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
@@ -20,6 +21,52 @@ const Search = () => {
   };
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={600}
+        sx={{ fontSize: { lg: "3rem", sx: "2.5rem" } }}
+        mb="1rem"
+        textAlign="center"
+      >
+        Check Out These Awesome <br />
+        Exercises .
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          className="textfield"
+          sx={{
+            input: {
+              color: "gray",
+              fontWeight: "700",
+              textAlign: "center",
+              border: "none",
+              lineHeight: "1.5",
+            },
+            width: { lg: "1000px", xs: "350px" },
+          }}
+          height="70px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#f94144",
+            height: "57px",
+            textTransform: "none",
+            color: "#fff",
+            width: { lg: "175px", xs: "80px" },
+
+            fontSize: { lg: "20px", xs: "14px" },
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+
+
       <Typography
         fontWeight={600}
         sx={{ fontSize: { lg: "3rem", sx: "2.5rem" } }}
