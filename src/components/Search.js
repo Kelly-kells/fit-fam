85ac450cd7f640fe714f45ed16f028981d40a8cd
unknown <kellskelechi@gmail.com>
2022-07-22 22:07:14 +0100

    fixes

--- src/components/Search.js
+++ src/components/Search.js
@@ -65,52 +65,6 @@ const Search = () => {
           Search
         </Button>
       </Box>
-
-
-      <Typography
-        fontWeight={600}
-        sx={{ fontSize: { lg: "3rem", sx: "2.5rem" } }}
-        mb="1rem"
-        textAlign="center"
-      >
-        Check Out These Awesome <br />
-        Exercises .
-      </Typography>
-      <Box position="relative" mb="72px">
-        <TextField
-          className="textfield"
-          sx={{
-            input: {
-              color: "gray",
-              fontWeight: "700",
-              textAlign: "center",
-              border: "none",
-              lineHeight: "1.5",
-            },
-            width: { lg: "1000px", xs: "350px" },
-          }}
-          height="70px"
-          value={search}
-          onChange={(e) => setSearch(e.target.value.toLowerCase())}
-          placeholder="Search Exercises"
-          type="text"
-        />
-        <Button
-          className="search-btn"
-          sx={{
-            bgcolor: "#f94144",
-            height: "57px",
-            textTransform: "none",
-            color: "#fff",
-            width: { lg: "175px", xs: "80px" },
-
-            fontSize: { lg: "20px", xs: "14px" },
-          }}
-          onClick={handleSearch}
-        >
-          Search
-        </Button>
-      </Box>
     </Stack>
   );
 };
