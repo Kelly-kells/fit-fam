a770657302df0e89e9a807cecea17559c1259a5a
unknown <kellskelechi@gmail.com>
2022-07-27 10:09:06 +0100

    imageData

---
+++ src/components/ImageData.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ImageData = [
+ {
+    id: 1,
+    img: require("./asset/back.png"),
+    
+ },
+ {
+    id: 2,
+    img: require("./asset/dumbellIcon.png"),
+    
+ }
+
+]
+  
+
+
+export default ImageData
